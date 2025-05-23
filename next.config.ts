93c77fa640b179134099c1f46a36021a93bb1ec0
박찬진 <68864422+developerChans@users.noreply.github.com>
2025-05-23 18:20:48 +0900

    fix:url

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+  },
   /* config options here */
 };
 
